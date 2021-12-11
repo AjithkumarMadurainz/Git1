e07806778e5b4cdcbf61eca1342532ee0ac596fc
AjithkumarmadurainZ <ajith9319@gmail.com>
2021-12-11 18:53:10 +0530

    git1

---
+++ src/org/git/Git1.java
@@ -0,0 +1,12 @@
+package org.git;
+
+public class Git1 {
+	private void add() {
+		System.out.println("Addall");
+
+	}
+	public static void main(String[] args) {
+		Git1 g =new Git1();
+		g.add();
+	}
+}
